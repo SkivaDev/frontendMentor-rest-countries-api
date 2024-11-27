ac579d45952f1a1f540b616bda0199efab99f334
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-11-27 11:19:52 -0500

    style(SkeletonHome): :lipstick: update tailwind styles for responsive mobile

--- components/SkeletonHome/SkeletonHome.tsx
+++ components/SkeletonHome/SkeletonHome.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const SkeletonHome = () => {
   return (
     <main className="flex justify-center items-center w-full pt-[50px]">
-      <div className="text-color-text w-full max-w-[90rem] px-[80px]">
-        <section className="flex justify-between items-center w-full">
+      <div className="text-color-text w-full max-w-[90rem] px-[22px] md:px-[80px]">
+        <section className="flex flex-col md:flex-row items-start md:items-center justify-between w-full gap-6">
           {/* Skeleton para FilterNameCountry */}
           <div className="flex items-center gap-3 border rounded-lg px-[28px] py-[15px] shadow-md w-full max-w-[480px] bg-gray-200 animate-pulse">
             <div className="w-6 h-6 bg-gray-300 rounded-full" />
@@ -12,7 +12,7 @@ const SkeletonHome = () => {
           </div>
 
           {/* Skeleton para FilterRegions */}
-          <div className="relative inline-block w-[200px] shadow-md">
+          <div className="relative inline-block w-full max-w-[200px] shadow-md">
             <div className="flex justify-between items-center w-full bg-gray-200 border border-gray-300 rounded-md px-[21px] py-[15px] animate-pulse">
               <div className="w-3/4 h-4 bg-gray-300 rounded-md" />
               <div className="w-4 h-4 bg-gray-300 rounded-full" />
@@ -22,7 +22,7 @@ const SkeletonHome = () => {
 
         {/* Skeleton para la lista de tarjetas de países */}
         <section>
-          <div className="countries-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[75px] gap-y-[73px] mt-[47px]">
+          <div className="countries-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 md:gap-x-[75px] gap-y-10 md:gap-y-[73px] mt-[47px]">
             {Array.from({ length: 8 }).map((_, index) => (
               <div
                 key={index}
